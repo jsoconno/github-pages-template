39c32c5d37e839a6e0a94e05422e1945ffec35d7
Justin O'Connor <justinoconnor@justins-air.lan>
2023-03-31 22:12:57 -0400

    fixed issues with search bar disappearing when the screen is smaller than 780 px wide

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,27 +13,13 @@
       </svg>
       <span class="header__name">{{ name }} <b>Documentation</b></span>
     </div>
-    <nav class="nav"
-      v-if="isVisible || isDesktop"
-    >
-        <svg
-          ref="nav__close"
-          class="nav__icon nav__icon--close"
-          tabindex="0"
-          @keyup.enter="closeNavigation"
-          @click="closeNavigation" 
-          viewBox="0 0 100 100" 
-          xmlns="http://www.w3.org/2000/svg"><path d="M77.6 21.1l-28 28.1-28.1-28.1-1.9 1.9 28 28.1-28 28.1 1.9 1.9L49.6 53l28 28.1 2-1.9-28.1-28.1L79.6 23z"/></svg>
-        <ul class="nav__list">
-          <router-link
-            v-for="item in list"
-            :key="item.path"
-            :to="item.path"
-            class="nav__item"
-          > {{ item.name }}
-          </router-link>
-        </ul>
-    </nav>
+    <div class="nav__search">
+      <input class="nav__search-input" placeholder="Type to search..." v-model="searchKeywords" @keyup="search"/>
+      <svg v-if="searchResultsVisible" class="nav__search-icon" tabindex="0" 
+        @click="reset" 
+        @keyup.enter="reset" 
+        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><path d="M26 0C11.664 0 0 11.663 0 26s11.664 26 26 26 26-11.663 26-26S40.336 0 26 0zm0 50C12.767 50 2 39.233 2 26S12.767 2 26 2s24 10.767 24 24-10.767 24-24 24z"/><path d="M35.707 16.293a.999.999 0 0 0-1.414 0L26 24.586l-8.293-8.293a.999.999 0 1 0-1.414 1.414L24.586 26l-8.293 8.293a.999.999 0 1 0 1.414 1.414L26 27.414l8.293 8.293a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414L27.414 26l8.293-8.293a.999.999 0 0 0 0-1.414z"/></svg>
+    </div>
   </header>
 </template>
 
@@ -47,17 +33,23 @@ export default {
       isVisible: false,
       isDesktop: false,
       name: ConfigManager.getBaseConfig().appName,
-      list: ConfigManager.getBaseConfig().headerNavigation
+      list: ConfigManager.getBaseConfig().headerNavigation,
+      content: []
     }
   },
-  created: function () {
+  created () {
+    ConfigManager.getStatus()
+      .then((data) => {
+        if (data !== undefined) this.content = data
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
+
     if (window.innerWidth >= 780) {
       this.isDesktop = true
     }
     window.addEventListener('resize', this.handleResize)
-  },
-  beforeDestroy: function () {
-    window.removeEventListener('resize', this.handleResize)
   },
   methods: {
     handleResize (event) {
@@ -72,6 +64,61 @@ export default {
     },
     closeNavigation (event) {
       this.$emit('toggleMenu')
+    },
+    async search ($event) {
+      if (!this.content.length) {
+        try {
+          this.content = await ConfigManager.getStatus()
+        } catch (error) {
+          console.error('Error fetching content', error)
+        }
+      }
+      if (this.searchKeywords) {
+        let query = Object.assign({}, this.$route.query, { search: this.searchKeywords })
+        this.$router.push({ query })
+
+        this.searchResultsVisible = true
+        let filter = 'tag:'
+        if (this.searchKeywords.indexOf('tag:') > -1) {
+          this.searchResults = this.content.filter(result => {
+            let tags = (result.tags || []).map((tag) => {
+              return tag.toLowerCase()
+            })
+
+            let tagExists = false
+
+            for (let i = 0; i < tags.length; i++) {
+              if (tags[i].indexOf(this.searchKeywords.toLowerCase().replace(filter, '')) > -1) {
+                tagExists = true
+              }
+            }
+
+            return tagExists
+          })
+        } else {
+          this.searchResults = this.content.filter(result => {
+            return (result.text || '').toLowerCase().indexOf(this.searchKeywords.toLowerCase()) > -1 ||
+              (result.name || '').toLowerCase().indexOf(this.searchKeywords.toLowerCase()) > -1
+          })
+          this.$emit('updateSearchResults', this.searchResults)
+        }
+      } else {
+        let query = this.$route.query
+        delete query.search
+        this.$router.push({ query })
+        this.reset()
+      }
+      this.$emit('updateSearchKeywords', this.searchKeywords)
+    },
+    searchTopic (tag) {
+      let filter = `tag:${tag}`
+      let query = Object.assign({}, this.$route.query, { search: filter })
+      this.searchKeywords = filter
+      this.search()
+      this.$router.push({ query })
+    },
+    reset () {
+      this.$emit('updateSearchKeywords', '')
     }
   }
 }
